21a2557c748fe500a6a18b23f40887af895c20b4
Jezv95 <138320134+Jezv95@users.noreply.github.com>
2025-06-24 01:42:20 +0000

    refactorizacion, impresion enHtml

--- src/app.js
+++ src/app.js
@@ -7,24 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
- let noun = ['jogger', 'racoon'];
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
 
-
-
-for (let p = 0; p < pronoun.length; p++){
-for (let a = 0; a < adj.length; a++){
-for (let n = 0; n < noun.length; n++){
-console.log("www." + pronoun[p] + adj[a] + noun[n] + ".com");
-}
- }
-}
-
-
+    for (let p = 0; p < pronoun.length; p++){
+        for (let a = 0; a < adj.length; a++){
+            for (let n = 0; n < noun.length; n++){
+                 let dominio = "<li>"+ "www." + pronoun[p] + adj[a] + noun[n] + ".com" + "</li>";
+                document.getElementById("domain").innerHTML += dominio
+            }
+        }
+    }
+   
 }
   
-
 
 
 
